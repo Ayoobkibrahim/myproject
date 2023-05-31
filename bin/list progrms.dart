5ef74762ec01d8a10ebbe64bca42105cd6e50f97
Ayoobkibrahim <ayoobkibrahim01@gmail.com>
2023-05-31 13:31:40 +0530

    commit

---
+++ bin/list progrms.dart
@@ -0,0 +1,10 @@
+void main(){
+   var list =[1,-3,7,9,0,-6,4,-2,0,10,-8,5];
+   int sum=0;
+   list.forEach((element) {
+     sum += element;
+   });
+   print(sum);
+
+
+}
---
+++ bin/listex.dart
@@ -0,0 +1,17 @@
+void main(){
+  var list =[1,-3,7,9,0,-6,4,-2,0,10,-8,5];
+  int esum = 0;
+  int osum = 0;
+
+  list.forEach((element) {
+    if(element%2==0){
+    esum+= element;
+  }else {
+    osum+=element;
+  }
+  });
+  print(esum);
+  print(osum);
+
+
+}
