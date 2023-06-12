e310c16ac4aeac4aebe789333522d67c9fc5373d
Ayoobkibrahim <ayoobkibrahim01@gmail.com>
2023-06-12 12:15:00 +0530

    commit

--- bin/oops/encapsulation.dart
+++ bin/oops/encapsulation.dart
@@ -1,4 +1,38 @@
 class MyDatas{
   String? name;
+  int? age;
+  int? phone;
+  String? email;
+///setter function for setting values to the instance variables
+  set data1(String name){
+    this.name=name;
+
+  }
+
+  set data2(int age){
+    this.age=age;
+  }
+  set data3(int phone) {
+    this.phone = phone;
+  }
+  set data4(String email) {
+    this.email = email;
+  }
+///if a class contains only setter functions and instance variables
+///then the class is write only class
+
+  String? get getname{
+    return name;
+  }
+  int? get getage {
+    return age;
+  }
+  int? get getphone {
+    return phone;
+  }
+  String? get getemail {
+    return email;
+  }
+
 
 }
