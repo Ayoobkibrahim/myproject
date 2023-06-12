e9da5e8069de37746f288362aebb558ef447f203
Ayoobkibrahim <ayoobkibrahim01@gmail.com>
2023-06-12 13:01:15 +0530

    commit

---
+++ bin/oops/assertionEx.dart
@@ -0,0 +1,11 @@
+///syntax:assert(condition,"messege");
+///used in application development stage, it is a debugging tool
+void main(){
+  int age=10;
+  // if(age<18){
+  //   print("not a valid age");
+  //
+  // }
+  assert(age<18, "age should be >=18");
+  print("Thank you");
+}
